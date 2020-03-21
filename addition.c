a37ab899957c97a29eb99d348b52aedb8d113202
minate <minatecarry@gmail.com>
2020-03-21 20:46:38 +0530

    Addition Program

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// #include<conio.h>
+void main()
+{
+	int num1, num2, res;
+	printf("Enter 2 numbers for addition\nFirst number: ");
+	scanf("%d", &num1);
+	printf("Second Number: 	");
+	scanf("%d", &num2);
+	res = num1 + num2;
+	printf("Addition gives %d\n", res);
+}
