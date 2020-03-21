d7cb3cb06c96781d16e19bc9a0ecbbcb7259fc17
minate <minatecarry@gmail.com>
2020-03-21 20:47:41 +0530

    Substraction Program

---
+++ substraction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1, num2, res;
+	printf("Enter two values to Substract\n");
+	printf("Enter the greater number: \n");
+	scanf("%d", &num1);
+	printf("Enter other number: \n");
+	scanf("%d", &num2);
+	res = num1 + num2;
+	printf("Substraction gives %d\n", res);
+}
