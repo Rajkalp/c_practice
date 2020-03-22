39c0fe4c505ea41426cf4bc3c2c18d4ac1f1b07c
minate <minatecarry@gmail.com>
2020-03-22 20:36:16 +0530

    Program to find area of square

---
+++ squarearea.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+	int A, l;
+	printf("Enter length of any one side of square to find area: ");
+	scanf("%d", &l);
+	A = l * l;
+	printf("Area of square is %d\n", A);
+}
