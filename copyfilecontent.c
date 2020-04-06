473c5747a27871f687f2ae6a47aba5a7a741df8b
Rajkalp <minatecarry@gmail.com>
2020-04-06 11:02:12 +0530

    Program to copy file content

---
+++ copyfilecontent.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+	FILE *p1, *p2;
+	char ch;
+	p1 = fopen("example2.c", "r");
+	p2 = fopen("example3.c", "w+");
+
+	if (p1 == NULL)
+	{
+		printf("File not found\n");
+	}
+	else{
+		while(1){
+			if (ch == EOF)
+			{
+				printf("Done!!\n");
+				break;
+			}
+			ch = getc(p1);
+			putc(ch, p2);
+		}
+	}
+}
